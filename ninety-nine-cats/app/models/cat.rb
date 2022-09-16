bef6f41d3ebb64403668f4db9bc5e17375173f75
Vincent Caracol <vcaracol@ymail.com>
2022-09-16 14:13:01 -0700

    working on phase1

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,16 +1,20 @@
+require "action_view"
+
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
     CAT_COLORS = ['cream','black','white']
     validates :birth_date, :color, :name, :sex, presence: true
     validates :color, inclusion: { in: CAT_COLORS}
-    validates :gender, inclusion: { in: ['M', 'F']}
+    validates :sex, inclusion: { in: ['M', 'F']}
 
     validate :birth_date_cannot_be_future
     
 
     def birth_date_cannot_be_future
-        if birth_date.present? && expiration_date > Date.today
+        if birth_date.present? && birth_date > Date.today
             errors.add(:birth_date, "can't be in the past")
-          end
         end
       
     end
